52e4faf72024bd4ec3533a438c7ddd21d6fb1967
francozp <31630191+francozp@users.noreply.github.com>
2018-12-16 19:23:21 -0300

    Lectura pacientes.JSON

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,14 +18,27 @@ public class Main {
         JSONParser parser = new JSONParser();
         JSONObject iter;
         JSONObject iter1;
+        JSONObject iter2;
+        JSONObject iter3;
+        JSONObject iter4;
+        JSONObject iter5;
         doctor medico;
         enfermero enf;
         paramedico param;
         requerimiento req;
+        paciente pac;
         List<doctor> listaDoctores= new ArrayList<doctor>();
         List<enfermero> listaEnfermeros= new ArrayList<enfermero>();
         List<paramedico> listaParamedicos= new ArrayList<paramedico>();
         List<requerimiento> listaRequerimientos= new ArrayList<requerimiento>();
+        List<paciente> listaPacientes = new ArrayList<paciente>();
+        List<String> enfermedades = new ArrayList<String>();
+        List<String> procAsignados = new ArrayList<String>();
+        List<String> procCompletados = new ArrayList<String>();
+        List<String> examRealizados = new ArrayList<String>();
+        List<String> examNorealizados = new ArrayList<String>();
+        List<String> medRecetados = new ArrayList<String>();
+        List<String> medSuministrados = new ArrayList<String>();
         try{
             Object obj = parser.parse(new FileReader("data/funcionarios.JSON"));
             JSONObject jsonObject = (JSONObject) obj;
@@ -56,8 +69,6 @@ public class Main {
             Iterator<JSONObject> iteratorReq = requerimientos.iterator();
             while (iteratorReq.hasNext()) {
                 iter = iteratorReq.next();
-                System.out.println(iter.get("id"));
-                System.out.println(iter.get("cargo"));
                 Iterator<JSONObject> pacientes =( (JSONArray) iter.get("pacientes")).iterator();
                 HashMap<String, String> procedimientos = new HashMap<>(); 
                 while (pacientes.hasNext()) {
@@ -69,6 +80,56 @@ public class Main {
                 }
                 req = new requerimiento((int) (long) iter.get("id"), (String)iter.get("cargo"),procedimientos);
                 listaRequerimientos.add(req);
+            }
+            obj = parser.parse(new FileReader("data/pacientes.JSON"));
+            jsonObject = (JSONObject) obj;
+            JSONArray pacientes = (JSONArray) jsonObject.get("Paciente");
+            Iterator<JSONObject> iteratorPac = pacientes.iterator();
+            System.out.println("Pacientes");
+            while (iteratorPac.hasNext()) { 
+                iter = iteratorPac.next();
+                System.out.println(iter.get("id"));
+                Iterator<JSONObject> iteratorDP =( (JSONArray) iter.get("datos personales")).iterator();
+                iter1 = iteratorDP.next();
+                JSONArray arreglo = (JSONArray) iter.get("enfermedades");
+                for(int i = 0; i < arreglo.size(); i++){
+                    enfermedades.add((String)arreglo.get(i));
+                }
+                Iterator<JSONObject> iteratorTRA =( (JSONArray) iter.get("tratamientos/procedimientos")).iterator();
+                iter3 = iteratorTRA.next();
+                arreglo = (JSONArray) iter3.get("asignados");
+                for(int i = 0; i < arreglo.size(); i++){
+                    procAsignados.add((String)arreglo.get(i));
+                }
+                iter3 = iteratorTRA.next();
+                arreglo = (JSONArray) iter3.get("completados");
+                for(int i = 0; i < arreglo.size(); i++){
+                    procCompletados.add((String)arreglo.get(i));
+                }
+                Iterator<JSONObject> iteratorEXA =( (JSONArray) iter.get("examenes")).iterator();
+                iter4 = iteratorEXA.next();
+                arreglo = (JSONArray) iter4.get("realizados");
+                for(int i = 0; i < arreglo.size(); i++){
+                    examRealizados.add((String)arreglo.get(i));
+                }
+                iter4 = iteratorEXA.next();
+                arreglo = (JSONArray) iter4.get("no realizados");
+                for(int i = 0; i < arreglo.size(); i++){
+                    examNorealizados.add((String)arreglo.get(i));
+                }
+                Iterator<JSONObject> iteratorMED =( (JSONArray) iter.get("medicamentos")).iterator();
+                iter5 = iteratorMED.next();
+                arreglo = (JSONArray) iter5.get("recetados");
+                for(int i = 0; i < arreglo.size(); i++){
+                    medRecetados.add((String)arreglo.get(i));
+                }
+                iter5 = iteratorMED.next();
+                arreglo = (JSONArray) iter5.get("suministrados");
+                for(int i = 0; i < arreglo.size(); i++){
+                    medSuministrados.add((String)arreglo.get(i));
+                }
+                pac = new paciente((String)iter1.get("nombre"),(String) iter1.get("rut"),Integer.parseInt((String)iter1.get("edad")),enfermedades,procAsignados,procCompletados,examRealizados,examNorealizados,medRecetados,medSuministrados);
+                listaPacientes.add(pac);
             }
             System.out.println("lo hice");
             
---
+++ src/main/hospital/paciente.java
@@ -0,0 +1,26 @@
+package main.hospital;
+import java.util.*;
+public class paciente{
+    private String  nombre;
+    private String  rut;
+    private int  edad;
+    private List<String> enfermedades;
+    private List<String> procAsignados;
+    private List<String> procCompletados;
+    private List<String> examRealizados;
+    private List<String> examNorealizados;
+    private List<String> medRecetados;
+    private List<String> medSuministrados;
+    public paciente(String name, String iden, int age, List<String> enf, List<String> procA,List<String> procC, List<String> examR, List<String> examN, List<String> medR, List<String> medS) {
+        nombre = name;
+        rut = iden;
+        edad = age;
+        enfermedades = enf;
+        procAsignados = procA;
+        procCompletados = procC;
+        examRealizados = examR;
+        examNorealizados = examN;
+        medRecetados = medR;
+        medSuministrados = medS;
+    }
+}
